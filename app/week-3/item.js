dd06e21a53f68a03526c0bf16f44cc01289ee829
dgonc99 <dominic.goncalves@edu.sait.ca>
2024-06-04 17:42:08 -0600

    Merge Error Correcting
    Fixed a couple errors in the last commit.

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,14 +1,10 @@
 const Item = ({ name, quantity, category }) => {
     return (
-        <li className="flex justify-between items-center p-4 border-b bg-slate-700" >
         <ul className="flex justify-between items-center p-4 border-b bg-slate-700" >
             <div className="flex flex-col">
-                <span className="font-bold text-lg text-white">{name}</span>
-                <span className="text-white">Buy {quantity} in {category}</span>
                 <li className="font-bold text-lg text-white">{name}</li>
                 <li className="text-white">Buy {quantity} in {category}</li>
             </div>
-        </li>
         </ul>
     );
 };
